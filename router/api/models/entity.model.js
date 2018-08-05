1be59bbf8650bbedc142083234c7c7c36c59892b
docg92 <diegoorlando5@hotmail.com>
2018-08-05 01:42:53 -0500

    entity model

--- router/api/models/entity.model.js
+++ router/api/models/entity.model.js
@@ -7,4 +7,13 @@ var entity = {
     count: Number
 }
 
-module.exports = entity;
+module.exports = entity;'use strict'
+
+var entity = {
+    type: String,
+    text: String,
+    relevance: Number,
+    count: Number
+}
+
+module.exports = entity; 
